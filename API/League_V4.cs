4d26d0ed4c0fb3906c2934e234fe1f32362b750f
Kadjuuu <32106468+Kadjuuu@users.noreply.github.com>
2020-02-05 23:59:18 +0100

    Initial commit

---
+++ API/League_V4.cs
@@ -0,0 +1,34 @@
+﻿using LeagueOfLegendsUI.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueOfLegendsUI.API
+{
+    public class League_V4 : API
+    {
+        public League_V4(string region) : base(region)
+        {
+        }
+
+        public List<PositionDTO> GetPositions(string summonerID)
+        {
+            string path = "league/v4/entries/by-summoner/" + summonerID;
+
+            var response = GET(GetURI(path));
+            string content = response.Content.ReadAsStringAsync().Result;
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return JsonConvert.DeserializeObject<List<PositionDTO>>(content);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
